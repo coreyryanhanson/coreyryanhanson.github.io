21a0390f6e3ef02a2c00f6b40df2b8bdb8c39d73
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2023-10-09 22:45:14 -0400

    fix image library location on submitted page, add lottie support, and update greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), lottie(), tailwind()]
 });
